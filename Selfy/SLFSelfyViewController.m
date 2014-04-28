9967b975bc9e44a2bfed0b42789b03fb37a68706
jdmgithub <jdm_email@yahoo.com>
2014-04-28 05:54:07 -0400

    Parse Data Pull
    Failed

--- Selfy/SLFSelfyViewController.m
+++ Selfy/SLFSelfyViewController.m
@@ -127,13 +127,13 @@
 -(void)newSelfy
 {
 
-    UIImage * image = [UIImage imageNamed:@"heart"];
+    UIImage * image = [UIImage imageNamed:@"magicSquare"];
     
     NSData * imageData = UIImagePNGRepresentation(image);
     
     
     // All from parse faq
-    PFFile * imageFile = [PFFile fileWithName:@"heart.png" data:imageData];
+    PFFile * imageFile = [PFFile fileWithName:@"magicSquare.png" data:imageData];
     
     PFObject * newSelfy = [PFObject objectWithClassName:@"userSelfy"];
     
--- Selfy/SLFTableViewController.m
+++ Selfy/SLFTableViewController.m
@@ -12,11 +12,11 @@
 #import "SLFSelfyViewController.h"
 #import "SLFNewNavigationController.h"
 
-@interface SLFTableViewController ()
+@interface SLFTableViewController : PFQueryTableViewController
 
 @end
 
-@implementation SLFTableViewController
+@implementation SLFTableViewController 
 
 
 {
@@ -144,6 +144,37 @@
 }
 
 
+//  Init for parse data import?
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom the table
+        
+        // The className to query on
+        self.className = @"Foo";
+        
+        // The key of the PFObject to display in the label of the default cell style
+        self.textKey = @"text";
+        
+        // Uncomment the following line to specify the key of a PFFile on the PFObject to display in the imageView of the default cell style
+        // self.imageKey = @"image";
+        
+        // Whether the built-in pull-to-refresh is enabled
+        self.pullToRefreshEnabled = YES;
+        
+        // Whether the built-in pagination is enabled
+        self.paginationEnabled = YES;
+        
+        // The number of objects to show per page
+        self.objectsPerPage = 25;
+    }
+    return self;
+}
+
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -235,6 +266,25 @@
     
 }
     
+
+
+
+
+
+// Parse data to fill table view?
+
+//- (PFTableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath object:(PFObject *)object
+//- (PFQuery *)queryForTable
+//
+
+
+
+
+
+
+
+
+
 
 
 
